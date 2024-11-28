cafe23fd5898bf7ea083797f40da7700237ed5cf
user <52472969+abdallah-eldali@users.noreply.github.com>
2024-11-28 03:33:52 -0500

    fix: using browser.* namespace and promises instead of chrome.*

--- src/popup.js
+++ src/popup.js
@@ -1,58 +1,77 @@
-document.getElementById('RemoveMessages').addEventListener('click', () => {
-  chrome.runtime.sendMessage({
-    action: 'REMOVE',
-  });
-});
+const removeMessagesButton = document.getElementById('RemoveMessages');
+const stopButton = document.getElementById('Stop');
+const delayInput = document.getElementById('Delay');
+const messageDiv = document.getElementById('Message');
+const lastClearedDiv = document.getElementById('LastCleared');
 
-document.getElementById('Stop').addEventListener('click', () => {
-  chrome.runtime.sendMessage({
-    action: 'STOP',
-  });
-});
+function disablePopupHtml() {
+  Array.from(document.getElementsByTagName('input'))
+       .forEach((input_element) => input_element.disabled = true);
+  Array.from(document.getElementsByTagName('button'))
+       .forEach((button_element) => button_element.disabled = true);
+  
+  messageDiv.textContent = `Disabled: The extension can only run in messenger.com`;
+}
 
-// If the active tab was previously cleared, update the popup with the info.
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  chrome.storage.local.get([tabs[0].url], (result) => {
-    if (!result || Object.keys(result).length === 0) return;
-    console.log('Found storage for url: ', result);
-    if ('lastCleared' in result[tabs[0].url]) {
-      document.getElementById('LastCleared').textContent =
-        'Last Cleared: ' + result[tabs[0].url]['lastCleared'];
-    }
-  });
-});
+function onError(error) {
+  console.error(`Error: ${error}`);
+  messageDiv.textContent = `Error: ${error}`;
+}
 
 // Make sure the user is using messenger.com.
 const messengers = [
   'https://www.messenger.com',
-  'http://www.messenger.com',
-  'https://messenger.com/',
-  'http://messenger.com/',
+  'https://messenger.com/'
 ];
-chrome.tabs.query(
-  {
-    active: true,
-    currentWindow: true,
-  },
-  (tabs) => {
-    const url = tabs[0].url;
-    if (!messengers.some((m) => url.startsWith(m))) {
-      alert(
-        `ERROR: Shoot the Messenger must be used on messenger.com, currently on ${url}. This extension will not work as intended.`,
-      );
-    }
-  },
-);
 
-document.getElementById('Delay').addEventListener('input', (e) => {
-  chrome.runtime.sendMessage({
-    action: 'UPDATE_DELAY',
-    data: e.target.value,
-  });
+browser.tabs.query({active: true, currentWindow: true})
+            .then((tabs) => {
+              if (!messengers.some((m) => tabs[0].url.startsWith(m))){
+                disablePopupHtml();
+              } else {
+                browser.tabs.sendMessage(tabs[0].id, { 
+                  action: 'UPDATE_DELAY',
+                  data: delayInput.value
+                });
+              }
+            })
+            .catch(onError);
+
+
+removeMessagesButton.addEventListener('click', () => {
+  browser.tabs.query({ active: true, currentWindow: true})
+              .then((tabs) => {
+                browser.tabs.sendMessage(tabs[0].id, {action: 'REMOVE'}); 
+              })
+              .catch(onError);
 });
 
-// Send over defaults to clear whatever the prior state is from local storage.
-chrome.runtime.sendMessage({
-  action: 'UPDATE_DELAY',
-  data: document.getElementById('Delay').value,
+stopButton.addEventListener('click', () => {
+  browser.tabs.query({ active: true, currentWindow: true})
+              .then((tabs) => {
+                browser.tabs.sendMessage(tabs[0].id, {action: 'STOP'}); 
+              })
+              .catch(onError);
 });
+
+delayInput.addEventListener('input', (e) => {
+  browser.tabs.query({ active: true, currentWindow: true})
+              .then((tabs) => {
+                browser.tabs.sendMessage(tabs[0].id, { 
+                  action: 'UPDATE_DELAY',
+                  data: e.target.value
+                });
+              })
+              .catch(onError);
+});
+
+browser.tabs.query({ active: true, currentWindow: true })
+            .then((tabs) => browser.storage.local.get([tabs[0].url]))
+            .then((result) => {
+              if (!result || Object.keys(result).length === 0) return;
+              console.log('Found storage for url: ', result);
+              if ('lastCleared' in result[tabs[0].url]) {
+                lastClearedDiv.textContent = `Last Cleared: ${result[tabs[0].url]['lastCleared']}`;
+              }
+            })
+            .catch(onError);
